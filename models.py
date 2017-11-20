ac821a76bde1b3e5b49b8be45a14dd9bb8c2695b
Romka Elberg <elberg2002@gmail.com>
2017-11-20 21:02:16 +0300

    Добавлена модель поиска

--- models.py
+++ models.py
@@ -1,6 +1,7 @@
 import json
 import ipfsapi
 import io
+import re
 
 api = ipfsapi.connect('127.0.0.1', 5001)
 
@@ -52,7 +53,7 @@ class Users(object):
 
 class Entry(object):
     def check(self, login, entry):
-        self.user_exist = User.check(login)
+        self.user_exist = User.check(self, login)
         if self.user_exist is True:
             self.entry = json.dumps(api.files_ls("/user/%s/entry" % login))
             self.entry = json.loads(self.entry)['Entries']
@@ -64,6 +65,7 @@ class Entry(object):
                 return True
             else:
                 return False
+    
     def get(self, login, header):
         if self.check(login, header) is True:
             self.entry = api.files_read("/user/%s/entry/%s" % (login, header))
@@ -92,3 +94,20 @@ class All_Entries(object):
             self.current_entry = api.files_read(self.all_entries_in_json[i]['path'])
             self.all_entries_in_list.append(self.current_entry.decode())
         return self.all_entries_in_list
+
+class Search(object):
+    def __init__(self, query):
+        self.query = query
+    
+    def get(self):
+        users = Users()
+        all_users = users.get()
+        all_users_len = len(all_users)
+        all_completed_users = []
+        pattern = re.compile(self.query)
+        for i in range(all_users_len):
+            result = pattern.findall(all_users[i])
+            result_len = len(result)
+            if result_len is not 0:
+                all_completed_users.append(all_users[i])
+        return json.dumps(all_completed_users)
