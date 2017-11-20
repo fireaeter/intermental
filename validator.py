5a4f5d94deacb597a65f156a4704c75ec3038ec5
Romka Elberg <elberg2002@gmail.com>
2017-11-20 23:06:27 +0300

    Добавлен валидатор записи

--- validator.py
+++ validator.py
@@ -1,3 +1,4 @@
+from sys import getsizeof
 from marshmallow import (
     Schema, pre_load, pre_dump, post_load, validates_schema,
     validates, fields, ValidationError
@@ -14,3 +15,15 @@ class UserSchema(Schema):
         if len(data['password']) < 8:
             raise ValidationError('password must be longer than 8.')
 
+
+class EntrySchema(Schema):
+    header = fields.String(required=True)
+    content = fields.String(required=True)
+    @validates_schema
+    def validate_entry_data(self, data):
+        header_size = getsizeof(data['header'])
+        if header_size > 255:
+            raise ValidationError('header must be smaller than 255')
+        content_size = getsizeof(data['content'].encode())
+        if content_size > 64000:
+            raise ValidationError('content must be smaller than 64kb')            
